edc0c40ae1088e3910bb5b304c3662c9fd552f58
Sabina Muskietorz <sabinamuskietorz@gmail.com>
2021-01-14 00:21:57 +0000

    first commit

--- components/adminnav.php
+++ components/adminnav.php
@@ -7,7 +7,6 @@
 		<li><a href="deletecategory.php">Delete category</a></li>
 		<li><a href="addarticle.php">Add article</a></li>
 		<li><a href="deletearticle.php">Delete article</a></li>
-		<li><a href="editarticle.php">Edit article</a></li>
 	</ul>
 </nav>
 <article>
